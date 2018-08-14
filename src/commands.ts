4f4d350b325bb50f7fdb61ed84c03d7a88b5a1c9
alexandre <amagnomonteiro@gmail.com>
2018-08-14 00:50:30 -0300

    Revert "first commit"
    This reverts commit c4ebf5c98bef41465fedae428ee359a068a9974b.

--- src/commands.ts
+++
@@ -1,8 +0,0 @@
-import { execSync } from 'child_process';
-import { config } from './config';
-
-export const runCommandTest = () => {
-  const command  = execSync(`cd ${config.blogDirectory} && npm run build`);
-
-  console.log(command.toString('utf8'));
-};
--- src/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare module '@dschau/create-gatsby-blog-post' {
-  export function createPost(postName: string, options?: 
-    { dry?: boolean, root?: string, date?: Date  }): Promise<any>
-}
